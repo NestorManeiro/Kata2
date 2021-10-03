740db7f85d0ebe6ffba07f5bc28921f1d3e39ea3
Nestor <Nestor@DESKTOP-LGULDM8>
2021-10-04 00:08:41 +0100

    Kata2 version3

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,7 +9,7 @@ public class Kata2 {
         int[] data = {1,3,5,6,7,8,3,6,8,1,3,2,4,3,6,7,8};
         Histogram histo = new Histogram(data);       
         Map<Integer,Integer> histogr = histo.getHistogram();        
-        histogr.keySet().forEach((key) -> {
+        histogr.keySet().forEach((key) -> { 
             System.out.println(key + "==>" + histogr.get(key));
         });
         
