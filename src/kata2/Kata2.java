fd439322b0b0a0de8a4ae9231a986bd13db65f16
Nestor <Nestor@DESKTOP-LGULDM8>
2021-10-04 15:22:33 +0100

    Kata2 version5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,9 +5,9 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer[] data = {1,3,5,6,7,8,3,6,8,1,3,2,4,3,6,7,8};
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histo = new Histogram(data);       
-        Map<Integer,Integer> histogr = histo.getHistogram();        
+        Map<String,Integer> histogr = histo.getHistogram();        
         histogr.keySet().forEach((key) -> { 
             System.out.println(key + "==>" + histogr.get(key));
         });
